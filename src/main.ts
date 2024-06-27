318daef9ca14b8c94199e0856ec538986b0761ac
marcopyre <marco.pyre@epsi.fr>
2024-06-27 14:33:12 +0200

    monitoring

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import tracer from './tracer';
 dotenv.config();
 
 async function bootstrap() {
+  await tracer.start();
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
